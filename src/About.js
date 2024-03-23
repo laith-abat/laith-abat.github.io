00e7df3fcb93361aa58176a5900729947e233e2f
Laith Abat <laithmhmdnour@gmail.com>
2024-03-23 06:55:29 -0700

    Testing

---
+++ src/About.js
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom"
+const About =()=>{
+      return (
+            <div>
+                  <Link to="/">Home</Link>
+                  <Link to="/about">About</Link>
+                  <Link to="/careers">Careers</Link>
+            </div>
+      )
+}
+export default About;
